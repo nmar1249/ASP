62450e29593825c188fa27ef92b845b01ee18736
nmar1249 <37256515+nmar1249@users.noreply.github.com>
2020-03-19 17:43:07 -0500

    - created HomeController

---
+++ WebApplication1/WebApplication1/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1
+{
+    public class HomeController : Controller 
+    {
+        ILog _log;
+
+        public HomeController(ILog log)
+        {
+            _log = log;
+        }
+
+        public IActionResult Index()
+        {
+            var services = this.HttpContext.RequestServices;
+            var log = (ILog)services.GetService(typeof(ILog));
+
+            _log.info("Executing /home/index.");
+
+            return View();
+        }
+
+        public IActionResult Index([FromServices] ILog log)
+        {
+            log.info("index method executing.");
+
+            return View();
+        }
+    }
+}
