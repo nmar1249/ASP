032d2a59baf7758155c512f7f319dadc1ebe3375
nmar1249 <37256515+nmar1249@users.noreply.github.com>
2020-04-01 16:32:10 -0500

    added GET methods to controller

--- TodoAPI/TodoAPI/Controllers/TodoController.cs
+++ TodoAPI/TodoAPI/Controllers/TodoController.cs
@@ -3,18 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using TodoAPI.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TodoAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/todo")]
     [ApiController]
     public class TodoController : Controller
     {
         private readonly TodoContext _context;
 
+        
+        //constructor
         public TodoController(TodoContext context)
         {
             _context = context;
@@ -26,5 +29,31 @@ namespace TodoAPI.Controllers
                 _context.SaveChanges();
             }
         }
+
+        #region GET METHODS
+
+        //GET: api/Todo
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        //GET: api/Todo/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if(todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        #endregion
+
     }
 }
