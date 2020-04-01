a61dd1cfbff935cd14cf5b3dd80ed501f5ced6a8
nmar1249 <37256515+nmar1249@users.noreply.github.com>
2020-04-01 18:18:06 -0500

    added default page added way to modify API via javascript

--- TodoAPI/TodoAPI/Startup.cs
+++ TodoAPI/TodoAPI/Startup.cs
@@ -45,6 +45,8 @@ namespace TodoAPI
                 app.UseHsts();
             }
 
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
